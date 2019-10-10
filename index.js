befeb2839074a261014e3752290d51acef680d61
Aakil Fernandes <aakilfernandes@gmail.com>
2019-10-10 19:18:48 -0400

    upgrade to use express

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
 const SignalingServer = require('pollenium-anemone/node/classes/SignalingServer').SignalingServer
-new SignalingServer(process.env.PORT || 5000, false)
+new SignalingServer(process.env.PORT || 5000)
