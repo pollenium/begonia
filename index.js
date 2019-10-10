ea471dc44c13c81d6a6379fd3ecaabc015bf6e64
Aakil Fernandes <aakilfernandes@gmail.com>
2019-10-10 16:53:34 -0400

    update anemone package name

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
-const SignalingServer = require('pollenium-client/node/classes/SignalingServer').SignalingServer
+const SignalingServer = require('pollenium-anemone/node/classes/SignalingServer').SignalingServer
 new SignalingServer(process.env.PORT || 5000, true)
