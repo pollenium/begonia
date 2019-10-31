95943f088eb109ee9b6578830acba4c82190a9f4
Aakil Fernandes <aakilfernandes@gmail.com>
2019-10-31 15:21:33 -0400

    lengthen restart

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const SignalingServer = require('pollenium-anemone/node/classes/SignalingServer').SignalingServer
 new SignalingServer(process.env.PORT || 5000)
 
-const restartFuzz = 10000 * Math.random()
+const restartFuzz = 600000 * Math.random()
 setTimeout(() => {
   console.log('restart')
   process.exit()
-}, 60000 + restartFuzz)
+}, 3600000 + restartFuzz)
