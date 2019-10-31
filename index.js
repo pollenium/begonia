12e118dbeb285f4a0b7975103dca08a8e343d9bb
Aakil Fernandes <aakilfernandes@gmail.com>
2019-10-31 15:00:16 -0400

    restart every hour

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 const SignalingServer = require('pollenium-anemone/node/classes/SignalingServer').SignalingServer
 new SignalingServer(process.env.PORT || 5000)
+
+const restartFuzz = 10000 * Math.random()
+setTimeout(60000 + restartFuzz, () => {
+  console.log('restart')
+  process.exit()
+})
