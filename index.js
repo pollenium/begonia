897fdf5b72de552acf6203f4f4befd25d793b0b1
Aakil Fernandes <aakilfernandes@gmail.com>
2019-10-31 15:05:29 -0400

    switch order

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const SignalingServer = require('pollenium-anemone/node/classes/SignalingServer'
 new SignalingServer(process.env.PORT || 5000)
 
 const restartFuzz = 10000 * Math.random()
-setTimeout(60000 + restartFuzz, () => {
+setTimeout(() => {
   console.log('restart')
   process.exit()
-})
+}, 60000 + restartFuzz)
