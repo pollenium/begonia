d5ad37047ca65cb5cc3be681ad970cdaecdb5652
Aakil Fernandes <aakilfernandes@gmail.com>
2020-02-05 19:28:30 -0500

    bump anemone

--- index.js
+++ index.js
@@ -1,8 +1,2 @@
-const SignalingServer = require('pollenium-anemone/node/classes/SignalingServer').SignalingServer
+const SignalingServer = require('pollenium-anemone').SignalingServer
 new SignalingServer(process.env.PORT || 5000)
-
-const restartFuzz = 600000 * Math.random()
-setTimeout(() => {
-  console.log('restart')
-  process.exit()
-}, 3600000 + restartFuzz)
